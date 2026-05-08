2c645dd821d4e0479618752f1539a2e917fc8057
markkiy <homoki.mark@ckik.hu>
2026-05-08 22:14:20 +0200

    fele kb

---
+++ SmartHub/Device.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHub
+{
+    public class Device
+    {
+        public string Name { get; set; }
+        public string Room { get; set; }
+        public int Value { get; set; }
+        public double Consumption { get; set; }
+        public bool IsTurnedOn { get; set; }
+
+        public Device(string name, string room, int value, double consumption, bool isTurnedOn)
+        {
+            Name = name;
+            Room = room;
+            Value = value;
+            Consumption = consumption;
+            IsTurnedOn = isTurnedOn;
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+
+        public double MonthlyUsage => Value * 100;
+
+    }
+}
--- SmartHub/MainWindow.xaml.cs
+++ SmartHub/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -14,11 +18,45 @@ namespace SmartHub
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        public ObservableCollection<Device> Devices { get; set; }
+
+        private Device? selectedDevice;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public Device? SelectedDevice {
+            get => selectedDevice;
+            set
+            {
+                selectedDevice = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SelectedDevice)));
+            }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            Devices = [];
         }
+
+        private void Load_Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog of = new();
+            var result = of.ShowDialog();
+            of.Filter = ".txt|*.txt";
+            if (result != true) return;
+            using StreamReader r = new(of.FileName);
+            while (!r.EndOfStream)
+            {
+                string[] temp = r.ReadLine()!.Split(";");
+                bool IsOn = temp[4] == "1";
+                Device d = new(temp[0], temp[1], int.Parse(temp[2]), double.Parse(temp[3]), IsOn);
+                Devices.Add(d);
+            }
+
+        }
+
     }
 }
